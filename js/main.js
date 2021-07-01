71f1266471edc106dee35c80d013a3b4bc92f6bf
CainStr <77696629+CainStr@users.noreply.github.com>
2021-07-01 08:07:52 +0300

    Сделал все, кроме проверки повторения адреса аватарки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const generateFractional = (min, max) => {
+const generateInt = (min, max) => {
   if (max > min && min >= 0) {
     return Math.round(Math.random() * (max - min) + min);
   }
@@ -7,9 +7,9 @@ const generateFractional = (min, max) => {
 };
 
 // eslint-disable-next-line no-console
-console.log(generateFractional(0, 10));
+console.log(generateInt(0, 10));
 
-const generateInt = (min, max) => {
+const generateFractional = (min, max) => {
   if (max > min && min >= 0) {
     return Number((Math.random() * (max - min) + min).toFixed(4));
   }
@@ -19,11 +19,12 @@ const generateInt = (min, max) => {
 
 
 // eslint-disable-next-line no-console
-console.log(generateInt(0, 10));
+console.log(generateFractional(35.65000, 35.70000));
 
 // author, объект — описывает автора. Содержит одно поле:
-let fractional = generateFractional(0,10);
+
 const addingZero = ()=> {
+  const fractional = generateInt(0,10);
   if(fractional< 10) {
     return `0${fractional}`;
   }
@@ -42,30 +43,70 @@ const offer= {
   title: ['Квартира с не самыми лучшими соседями', 'Лучше доплатить за кондиционер :)', 'Лучшее предложение',
     'Это место ты запомнишь навсегда', 'Вид на море', 'Обычное место', 'Самое дешевое место',
     'Кладовка и то больше', 'Иногда орет сосед', 'Вид на крематорий'],
-
-  address: '{{location.lat}}, {{location.lng}}',
-  price:((Math.random(0,))*1000000).toFixed(),
+  // address: {location: {lat: generateFractional(35.65000, 35.70000), lng:generateFractional(139.70000, 139.80000)}},
   type: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
-  rooms: ((Math.random(0,))*1000).toFixed(),
-  guests: generateFractional(1,5),
   checkin:['12:00', '13:00', '14:00'],
   checkout: ['12:00', '13:00', '14:00'],
   features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
 };
-const numberArrayOffer = generateFractional(0, offer.type.length-1);
-const createOffers =() => {
-
-  return {
-    offerTitle: offer.title[numberArrayOffer],
-    offerType :offer.type[numberArrayOffer],
-    offerRooms: offer.rooms,
-  };
+// const numberArrayOffer = generateInt(0, offer.type.length-1);
+console.log(offer.address);
+const  getRandomArrayElement = (elements) => {
+  return elements[generateInt(0, elements.length-1)];
 };
 
-const similarOffers = new Array(10).fill(null).map(()=>createOffers());
-console.log(similarOffers)
-console.log(offer.type[numberArrayOffer]);
-console.log(numberArrayOffer);
-console.log(offer.price);
-console.log(offer.rooms);
-console.log(offer.guests);
+const lat=(min, max) => (generateFractional(min, max));
+const lng = (min, max) => (generateFractional(min, max));
+lat(35.65000, 35.70000);
+console.log(lng(35.6500, 35.7000))
+const createOffers =() => ({
+  author: {
+    avatar:`img/avatars/user${addingZero()}.png`,
+  },
+  // location: {lat: `${lat()}`, lng:generateFractional(139.70000, 139.80000)},
+  offer:{
+    Title: getRandomArrayElement(offer.title),
+
+    Type :getRandomArrayElement(offer.type),
+    Rooms: ((Math.random(0))*1000).toFixed(),
+    Guests: generateInt(1,5),
+    Price:((Math.random(0))*1000000).toFixed(),
+    Checkin: getRandomArrayElement(offer.checkin),
+    Checkout: getRandomArrayElement(offer.checkin),
+    Features: getRandomArrayElement(offer.features),
+  },
+});
+
+// const similarOffers = new Array(10).fill(null).map(()=>createOffers());
+// const q =(min,max)=>(lat(min, max)) ;
+
+const similarOffers = new Array(10);
+for( let j = 0 ; j < similarOffers.length ; j++) {
+  similarOffers[j] = createOffers();
+}
+
+for(let i=0; i<similarOffers.length ; i++) {
+  const q= lat(35.65000, 35.70000);
+  const r =  lng(139.70000, 139.80000);
+  Object.assign(similarOffers[i].offer, {
+    address: q + ', ' + r,
+
+    // address:[lat(35.65000, 35.70000), lng(139.70000, 139.80000)],
+  });
+  Object.assign(similarOffers[i],{
+    location: {lat: q, lng: r},
+
+  });
+  console.log(similarOffers[i].offer.address)
+}
+
+
+// for( let d = 0 ; d < 10 ; d++) {
+//   if (similarOffers[d].avatar = similarOffers[d + 1].avatar) {
+//     delete similarOffers[d].avatar;
+//   }
+//
+//   console.log(createOffers());
+// }
+
+console.log(similarOffers);
