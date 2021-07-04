cd36839c00fdf31a8cda3a80003a038e3e4da869
CainStr <77696629+CainStr@users.noreply.github.com>
2021-07-04 03:36:12 +0300

    Убрал повторяющиеся числа в свойстве avatar, объекта similarOffers[i]

--- js/main.js
+++ js/main.js
@@ -1,36 +1,43 @@
+const maxArrayLength = 10;
+const minArrayLength = 0;
+const numbersAfterZero = 4;
+const guestsCountMin = 1;
+const guestsCountMax = 5;
+
 const generateInt = (min, max) => {
-  if (max > min && min >= 0) {
-    return Math.round(Math.random() * (max - min) + min);
+  if (max > min && min >= minArrayLength) {
+    return Number(Math.round(Math.random() * (max - min) + min));
   }
 
   throw new RangeError('Проверь числа');
 };
 
-const generateFractional = (min, max) => {
-  if (max > min && min >= 0) {
-    return Number((Math.random() * (max - min) + min).toFixed(4));
+const generateFractional = (min, max, afterTheSign) => {
+  if (max > min && min >= minArrayLength) {
+    return Number((Math.random() * (max - min) + min).toFixed(afterTheSign));
   }
 
   throw new RangeError('Проверь числа!');
 };
 
 const offer = {
-  title: ['Квартира с не самыми лучшими соседями', 'Лучше доплатить за кондиционер :)', 'Лучшее предложение',
+  titlesArray: ['Квартира с не самыми лучшими соседями', 'Лучше доплатить за кондиционер :)', 'Лучшее предложение',
     'Это место ты запомнишь навсегда', 'Вид на море', 'Обычное место', 'Самое дешевое место',
     'Кладовка и то больше', 'Иногда орет сосед', 'Вид на крематорий'],
-  type: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
-  checkin: ['12:00', '13:00', '14:00'],
-  checkout: ['12:00', '13:00', '14:00'],
-  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-  photos: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  typesArray: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+  checkinArray: ['12:00', '13:00', '14:00'],
+  checkoutArray: ['12:00', '13:00', '14:00'],
+  featuresArray: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  photosArray: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'],
+  descriptionArray: [' нет парковочного места',
+    ' происходят странные вещи',
+    ' врезаются перелетные птицы в стекло',
+    ' толстенные стены, здесь вам ничего не грозит'],
 };
 
-const maxArrayLength = 10;
-const minArrayLength = 0;
 const getRandomArrayElement = (elements) => generateInt(minArrayLength, elements.length - 1);
-
 
 const addingZero = () => {
   for (let numberAvatar = 0; numberAvatar < maxArrayLength; numberAvatar++) {
@@ -42,30 +49,29 @@ const addingZero = () => {
     }
   }
 };
-
+const commentForDescription = 'В добавок';
 const createOffers = () => ({
   offer: {
-    Title: offer.title[getRandomArrayElement(offer.title)],
-    Type: offer.type[getRandomArrayElement(offer.type)],
-    Rooms: ((Math.random()) * 1000).toFixed(),
-    Guests: generateInt(1, 5),
-    Price: ((Math.random()) * 1000000).toFixed(),
-    Checkin: offer.checkin[getRandomArrayElement(offer.checkin)],
-    Checkout: offer.checkout[getRandomArrayElement(offer.checkout)],
-    Features: offer.features[getRandomArrayElement(offer.features)],
-    Photos: offer.photos[getRandomArrayElement(offer.photos)],
+    Title: offer.titlesArray[getRandomArrayElement(offer.titlesArray)],
+    Description: `${commentForDescription},${offer.descriptionArray[getRandomArrayElement(offer.descriptionArray)]}`,
+    Type: offer.typesArray[getRandomArrayElement(offer.typesArray)],
+    Rooms: Number(((Math.random()) * 1000).toFixed()),
+    Guests: generateInt(guestsCountMin, guestsCountMax),
+    Price: Number(((Math.random()) * 1000000).toFixed()),
+    Checkin: offer.checkinArray[getRandomArrayElement(offer.checkinArray)],
+    Checkout: offer.checkoutArray[getRandomArrayElement(offer.checkoutArray)],
+    Features: offer.featuresArray[getRandomArrayElement(offer.featuresArray)],
+    Photos: offer.photosArray[getRandomArrayElement(offer.photosArray)],
   },
 });
-
-
 const similarOffers = new Array(maxArrayLength);
 for (let index = 0; index < similarOffers.length; index++) {
   similarOffers[index] = createOffers();
 
 }
 
-const lat = (min, max) => (generateFractional(min, max));
-const lng = (min, max) => (generateFractional(min, max));
+const lat = (min, max) => (generateFractional(min, max, numbersAfterZero));
+const lng = (min, max) => (generateFractional(min, max, numbersAfterZero));
 
 for (let index = 0; index < similarOffers.length; index++) {
   const locationLat = lat(35.65000, 35.70000);
@@ -81,6 +87,4 @@ for (let index = 0; index < similarOffers.length; index++) {
       avatar: `img/avatars/user${addingZero() + (index + 1)}.png`,
     },
   });
-
 }
-
